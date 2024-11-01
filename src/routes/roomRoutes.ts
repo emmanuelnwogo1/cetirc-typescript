3cb1621c2d703c04a73d91549e2e5c09725d4811
Waqas Raza <waqasraza123@gmail.com>
2024-11-02 00:42:58 +0500

    api/grant-access/school/

--- src/routes/roomRoutes.ts
+++ src/routes/roomRoutes.ts
@@ -3,7 +3,7 @@ import verifyToken from '../middlewares/authMiddleware';
 import { roomListController } from '../controllers/room/roomListController';
 import { roomDetailController } from '../controllers/room/roomDetailController';
 import { roomSmartLockController } from '../controllers/room/smartlock/roomSmartLockController';
-import { viewUserAccessInRoomController } from '../controllers/room/access/roomAccessController';
+import { createUserAccessController, viewUserAccessInRoomController } from '../controllers/room/access/roomAccessController';
 import { deleteUserAccessFromRoomController } from '../controllers/room/access/deleteUserAccessFromRoomController';
 
 const router = express.Router();
@@ -13,5 +13,6 @@ router.get('/rooms/:business_type/:pk', verifyToken, roomDetailController);
 router.post('/assign-smart-lock-to-room/:id', verifyToken, roomSmartLockController);
 router.get('/user-access/:room_id', verifyToken, viewUserAccessInRoomController);
 router.delete('/remove-access/:user_id', verifyToken, deleteUserAccessFromRoomController);
+router.post('/grant-access/:business_type', verifyToken, createUserAccessController);
 
 export default router;
--- src/services/room/smartlock/access/roomAccessService.ts
+++ src/services/room/smartlock/access/roomAccessService.ts
@@ -1,3 +1,5 @@
+import { SmartLock } from "../../../../models/SmartLock";
+import { User } from "../../../../models/User";
 import { UserSmartLockAccess } from "../../../../models/UserSmartLockAccess";
 
 export const getUserAccessInRoom = async (roomId: number | null, user: any) => {
@@ -45,6 +47,66 @@ export const getUserAccessInRoom = async (roomId: number | null, user: any) => {
             data: {
                 status: 'failed',
                 message: 'An error occurred while retrieving user access.',
+            },
+        };
+    }
+};
+
+interface GrantAccessData {
+    username: string;
+    smart_lock_device_id: string;
+    period: Date;
+}
+
+export const grantUserAccess = async (data: GrantAccessData, businessType: string, currentUser: any) => {
+    const { username, smart_lock_device_id, period } = data;
+
+    try {
+        
+        const userToGrant = await User.findOne({ where: { username } });
+        const smartLock = await SmartLock.findOne({ where: { device_id: smart_lock_device_id } });
+
+        if (!userToGrant) {
+            return {
+                statusCode: 404,
+                data: {
+                    status: 'failed',
+                    message: 'User not found.',
+                },
+            };
+        }
+
+        if (!smartLock) {
+            return {
+                statusCode: 404,
+                data: {
+                    status: 'failed',
+                    message: 'Smart lock not found.',
+                },
+            };
+        }
+
+        await UserSmartLockAccess.create({
+            user_id: userToGrant.id,
+            smart_lock_id: smartLock.id,
+            granted_by_id: currentUser.id,
+            period,
+        } as UserSmartLockAccess);
+
+        return {
+            statusCode: 201,
+            data: {
+                status: 'success',
+                message: 'Access granted successfully.',
+            },
+        };
+    } catch (error) {
+        console.error('Error granting user access:', error);
+        return {
+            statusCode: 500,
+            data: {
+                status: 'failed',
+                message: 'An error occurred while granting access.',
             },
         };
     }
